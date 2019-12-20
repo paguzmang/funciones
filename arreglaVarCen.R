184a861bfbc3e92019701b4066863bc2ac4db936
paguzmang <51097901+paguzmang@users.noreply.github.com>
2019-12-20 09:39:03 -0500

    Add files via upload

--- arreglaVarCen.R
+++ arreglaVarCen.R
@@ -1,4 +1,4 @@
-arreglaVarCen <- function(x, texto.cen = '<', res.x = F, nom.x = 'x'){
+arreglaVarCen <- function(x, texto.cen = '<', res.x = F, nom.x = 'x', reemplazo = c('mitad',  'na')){
   #
   # Descripcion de la funcion
   # Toma un vector de texto en el cual algunas entradas estan
@@ -16,6 +16,8 @@ arreglaVarCen <- function(x, texto.cen = '<', res.x = F, nom.x = 'x'){
   #         una columna con vector original?
   # nom.x = Nombre de la variable analizada para que
   #         salga etiquetada en el data.frame resultante
+  # reemplazo = Un valor de texto indicando por que se quiere hacer el reemplazo, si por
+  #   la 'mitad' del limite de deteccion (por defecto) o por 'na'.
   
   # Codigo:
   pos.cen       <- grep(pattern = texto.cen,  x = x)
@@ -26,7 +28,8 @@ arreglaVarCen <- function(x, texto.cen = '<', res.x = F, nom.x = 'x'){
   x.num <- as.numeric(y)
   x.lim <- unique(x.num[pos.cen])
   tab.res <- as.data.frame(table(x.num[pos.cen]))
-  x.num[pos.cen] <- x.num[pos.cen]/2   # reemplazo por la media
+  if(!reemplazo[1] %in% c('mitad',  'na') ) stop("remplazo debe ser uno entre 'mitad' o 'na'")
+  if(reemplazo[1] == 'mitad') x.num[pos.cen] <- x.num[pos.cen]/2 else  x.num[pos.cen] <- NA  # reemplazo por la media
   names(tab.res) <- c('lim', 'n_cen')
   tab.res$p_cen <- round(tab.res$n_cen/length(x)*100,2)
   tab.res$texto.cen = texto.cen
@@ -48,10 +51,22 @@ arreglaVarCen <- function(x, texto.cen = '<', res.x = F, nom.x = 'x'){
   )
   
   # Valor
-  # Entrega una lista con dos data.frame (datos y res.cen)
+  # Entrega una lista con dos data.frame (datos y res.cen).
+  # datos = contiene la nueva columna numerica y una columna logica que indica si
+  #   el dato estuvo por debajo del limite o no. Opcionalmente si res.x = T, entrega
+  #   tambien el vector original.
+  # res.cen = data.frame con resumen de lo encontrado con respecto a datos censurados
+  #   o por debajo del limite.
+  #   lim = limites de deteccion encontrados
+  #   n_cen = nro. de entradas encontradas con el formato '<LD'
+  #   p_cen = porcentaje de entradas encontradas con el formato '<LD'
+  #   texto.cen = El texto o signo encontrado que marca si un dato esta por debajo del limite.
   
   # Ejemplo de uso:
-  # x <- c(45.5, 34.5, '<5.5', '<  5.5', '<3.4', 32, 24)
-  # arreglaVarCen(x = x)
-  # arreglaVarCen(x = x, res.x = T)
+  # w <- c(45.5, 34.5, '<5.5', '<  5.5', '<3.4', 32, 24)
+  # arreglaVarCen(x = w)
+  # arreglaVarCen(x = w, res.x = T)
+  # arreglaVarCen(x = w, res.x = T, reemplazo = 'na')
+  # arreglaVarCen(x = w, res.x = T, reemplazo = 'nas')   # detiene y saca mensaje informativo
+  # arreglaVarCen(x = w, res.x = T, reemplazo = 'na', nom.x = 'NO2')   
 }
