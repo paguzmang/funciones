b2e2cc7ff12012c3c5c77a5ee679da868d701870
paguzmang <51097901+paguzmang@users.noreply.github.com>
2020-04-09 18:40:03 -0500

    Add files via upload

---
+++ shannon.R
@@ -0,0 +1,22 @@
+shannon <- function(x){
+  # Version: 1.0 | Autor: paguzmang
+  # Descripcion:
+  # Esta funcion calcula el indice de diversidad de shannon desde un
+  # vector de abundancias por especie.
+  
+  # Argumentos:
+  # x = vector numerico de abundancias por especie. No pueden haber abundancias
+  #     de cero.
+  
+  # Codigo:
+  n <-  sum(x)      # nro. total (n) de individuos en la muestra
+  pi <- x / n       # se calculan las proporciones de cada especie
+  -sum(pi*log(pi))  # se obtiene el indice de shannon (H') y se imprime
+  
+  # Valor:
+  # Esta funcion entrega un solo numero correspondiente al indice de shannon
+  
+  # Ejemplo de uso:
+  # ni <- c(20, 50, 12, 3)
+  # shannon(x = ni)
+}
