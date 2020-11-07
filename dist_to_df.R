150d187ef4e80aa5a6b2f74e9b825078bec403d4
paguzmang <51097901+paguzmang@users.noreply.github.com>
2020-11-07 11:38:55 -0500

    distancia a data.frame

---
+++ dist_to_df.R
@@ -0,0 +1,22 @@
+dist.to.df <- function(x) {
+  
+  # Descripcion:
+  # Funcion que transforma una matriz de distancia (generada con
+  # el comando "dist") en un data.frame. Tomado del enlace:
+  # https://stackoverflow.com/questions/23474729/convert-object-of-class-dist-into-data-frame-in-r
+  # Consultada el: 7-nov-2020
+  
+  # Argumentos:
+  # x = objeto de de clase "dist"
+  
+  # Codigo:
+  if (class(x) != "dist") stop("wrong input type")
+  A <- attr(x, "Size")
+  B <- if (is.null(attr(x, "Labels"))) sequence(A) else attr(x, "Labels")
+  if (isTRUE(attr(x, "Diag"))) attr(x, "Diag") <- FALSE
+  if (isTRUE(attr(x, "Upper"))) attr(x, "Upper") <- FALSE
+  data.frame(
+    row = B[unlist(lapply(sequence(A)[-1], function(x) x:A))],
+    col = rep(B[-length(B)], (length(B)-1):1),
+    dist = as.vector(x))
+}
