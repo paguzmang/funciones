810c16922a97087ce02cbcd22253cf8802fde97a
paguzmang <51097901+paguzmang@users.noreply.github.com>
2024-10-19 18:44:26 -0500

    Add files via upload

--- rad_lump.R
+++ rad_lump.R
@@ -19,9 +19,10 @@ rad_lump <- function(x, s_keep = NULL, decreasing = TRUE,
   # x = Datos. Debe ser uno de dos tipos:
   #
   #       - Vector numerico de abundancias por especie
-  #         ojala, este nombrado con el nombre o abreviacion de cada especie. La suma
-  #         de este vector debe dar la abundancia total (sum(x)) y su 
-  #         longitud la riqueza (length(x))
+  #         ojala, este nombrado con el nombre o abreviacion de cada especie. 
+  #         Este vector puede ser el resultado de usar los comandos 
+  #         'table' o 'xtabs' de R. La suma de este vector debe dar la 
+  #         abundancia total sum(x) y su longitud la riqueza length(x[x>0]).
   #
   #       - Un vector de texto o factor indicando el nombre de la especie para cada 
   #         individuo en la muestra. La longitud de este vector sera la abundancia total.
@@ -51,11 +52,11 @@ rad_lump <- function(x, s_keep = NULL, decreasing = TRUE,
   # Calculos
   # Se validan el tipo de dato en 'x':
   if(class(x) %in% c("character", "factor") ){
-    x <- table(x)
-    class(x) <- NULL
+    x <- table(as.character(x))
   }
+  class(x) <- NULL
   if(is.null(names(x))) names(x) <- paste0('sp', 1:length(x))  # si no hay nombres que lo asigne
-  x <- sort(x[x > 0], decreasing = decreasing)
+  x <- sort(x[x > 0], decreasing = T)
   k <- 1:length(x)
   s <- length(x)    # riqueza
   n <- sum(x)       # abundancia total
@@ -67,10 +68,22 @@ rad_lump <- function(x, s_keep = NULL, decreasing = TRUE,
   DF$f_acum <- cumsum(DF$f)
   DF$fr_acum <- cumsum(DF$fr)
   
+  # Funcion para ordenar el data.frame resultante
+  ordena_df <- function(df, decreasing = F){
+    if(isFALSE(decreasing)){
+      df <- df[nrow(df):1, ]
+      row.names(df) <- NULL
+      df
+    } else{
+      df
+    }
+  }
+  
   # En caso de que s_keep sea nulo o s_keep mayor o igual a s
   imprima_DF <- ifelse(is.null(s_keep), T, is.numeric(s_keep) & (s_keep >= s))
   if(imprima_DF){
-    DF$sp <- factor(DF$sp, levels = sp)
+    DF <- ordena_df(DF, decreasing = decreasing)
+    DF$sp <- factor(DF$sp, levels = DF$sp)
     DF   # se imprime
   } else{
     # Busqueda autmoatica de s_keep
@@ -90,9 +103,10 @@ rad_lump <- function(x, s_keep = NULL, decreasing = TRUE,
           fr_acum = sum(DF$fr)
         )
       DFr <- rbind(DF1, DF3)
-      DFr$sp <- factor(DFr$sp, levels = DFr$sp)
       u <- DFr$fr[nrow(DFr)] > DF$fr[1]   # se actualiza el valor de u
       }
+      DFr <- ordena_df(DFr, decreasing = decreasing)
+      DFr$sp <- factor(DFr$sp, levels = DFr$sp)
       DFr  # se imprime
     } else{
       # s_keep debe ser numerico y menor que s desde su definicion como argumento
@@ -107,10 +121,51 @@ rad_lump <- function(x, s_keep = NULL, decreasing = TRUE,
         fr_acum = sum(DF$fr)
       )
       DFr <- rbind(DF1, DF3)
-      u <- DFr$fr[nrow(DFr)] < DF$f[1]
+      DFr <- ordena_df(DFr, decreasing = decreasing)
       DFr$sp <- factor(DFr$sp, levels = DFr$sp)
       DFr  # se imprime
     }
   }
+  
+  # # Ejemplos
+  # # Simulacion de un vector de abundancias por especie:
+  # x <- sample(paste0("sp",  1:40))  # vector de nombres de especies
+  # d <- dexp(x = 1:40, rate = 0.08)  # vector de frecuencias relativas
+  # # simulacion de muestra de 500 individuos
+  # x <- sample(x, size = 500, replace = T, prob = d)  
+  # ab <- table(x)  # abundancias por especie
+  # sum(ab)  # debe dar 500
+  # length(ab)  # riqueza
+  # 
+  # # Se aplica la funcion rad_lump 
+  # res <- rad_lump(x = ab, s_keep = NULL)                    # no se agrupa
+  # str(res)  # revise la estructura del objeto
+  # res       # imprima el objeto
+  # rad_lump(x = ab, s_keep = NULL, decreasing = F)    # no se agrupa
+  # rad_lump(x = ab, s_keep = "auto")  # con determinacion automatica de s_keep
+  # res <- rad_lump(x = ab, s_keep = "auto", decreasing = F)  # con determinacion automatica de s_keep
+  # rad_lump(x = ab, s_keep = 50)      # no se agrupa porque s_keep > riqueza
+  # rad_lump(x = ab, s_keep = 10)      # se agrupan a partir de la especie 11 mas abundante
+  # rad_lump(x = ab, s_keep = 10, decreasing = F)      # se agrupan a partir de la especie 11 mas abundante
+  # rad_lump(x = x)   # tambien funciona desde datos de individuos por especie
+  # 
+  # # Grafico
+  # library(ggplot2)
+  # df <- rad_lump(x = ab, s_keep = 50, decreasing = F)  # con determinacion automatica de s_keep
+  # str(df)
+  # ggplot(df, aes(x = fr, y = sp)) + 
+  #   geom_col(fill = "tomato1") +
+  #   scale_x_continuous(expand = c(0,0)) +
+  #   labs(x = "Abundancia (%)", y = "Especies")
+  # 
+  # # RAD:
+  # ggplot(df, aes(x = k, y = fr)) + 
+  #   geom_path() +
+  #   labs(x = "rank de Especies", y = "Abundancia (%)")
+  # 
+  # # Grafico de abundancia acumulada
+  # ggplot(df, aes(x = k, y = fr_acum)) + 
+  #   geom_line() +
+  #   labs(x = "rank de Especies", y = "Abundancia acumulada (%)")
   
 }
