f4579d221cb08a1b7eb2b406d717f829b8266c78
paguzmang <51097901+paguzmang@users.noreply.github.com>
2024-11-27 11:19:23 -0500

    Add files via upload

--- estad_descrip.R
+++ estad_descrip.R
@@ -1,13 +1,14 @@
-estad.descrip <- function(x, r = NULL, var.en.filas = T, 
-                          est = c('n.m.s', 'n.m.s.cv', 'n.r.m.s', 'n.r.m.s.cv', 'todo', 'rob', 'quant'), 
-                          n.auto = T, n.dist = F, print = c('data.frame', 'tibble')){
+estad_descrip <- function(x, r = NULL, var.en.filas = TRUE, estad_forma = FALSE, 
+                          n.auto = TRUE, n.dist = FALSE, print_tibble = FALSE){
   
-  # Fecha: 24-feb-2020
+  # Fecha: 27-nov-2024
   # Autor: paguzmang
   
   # DESCRIPCION:
-  # Funcion que calcula combos de estadisticos descriptivos sobre un vector, o sobre una o 
-  # mas columnas de un data.frame. Permite pasarse por la funcion ddply (del paquete plyr).
+  # Funcion que calcula estadisticos descriptivos sobre un vector, o sobre una o 
+  # mas columnas de un data.frame. Calcula el numero de datos, el min, q25, q50, q75, max,
+  # media, desv. estandar, coef. de variacion el sesgo y la kurtosis.
+  # Permite pasarse por la funcion ddply (del paquete plyr).
   # para hacer calculos por grupos (split - apply - combine). Ver ejemplos.
   
   # ARGUMENTOS:
@@ -19,20 +20,12 @@ estad.descrip <- function(x, r = NULL, var.en.filas = T,
   #     analizaran. Si no se usa (se deja NULL), se seleccionan automaticamente todas las columnas 
   #     numericas del data.frame. Si no hay ninguna, la funcion se detiene y genera un mensaje de error.
   #
-  # var.en.filas = Logico. Si length(r) > 1 (dos o mas columnas), indica si las variables se ubicaran
+  # var.en.filas = Logico. Si length(r) > 1 (dos o mas columnas), indica si, en el resultado, las variables se ubicaran
   #     en las filas (por defecto) y los estadisticos en las columnas o viceversa.
   #
-  # est = Indica que combinacion ('combo') de estadisticos se desea:
-  #       n.m.s = tamano de muestra (n), media (m) y desviacion estandar (s) (opcion por defecto)
-  #       n.m.s.cv = n, m, s y coef. de variacion (cv, en porcentaje)
-  #       n.r.m.s = n, rango (r: min y max), m, s
-  #       n.r.m.s.cv = n, r, m, s y cv.
-  #       todo = n, m, s, r, cuartiles (quant), media geometrica (mg), desviacion mediana absoluta (mad), 
-  #              sesgo, kurtosis (kurt), y coef. de variacion robusto (cvr = mad / q50 *100)
-  #       rob = n y estadisticos robustos (quant, mg, mad, cvr)
-  #       quant = n, r, cuartiles.
-  #       En todos los casos, si hay valores perdidos, la funcion los saca antes del calculo. Ademas
-  #       el n reportado depende del argumento n.auto.
+  # estad_forma = Si TRUE, se imprimen en los estadisticos, el sesgo y la kurtosis, calculados desde
+  #     las funciones e1071::skewness y e1071::kurtosis. Consulte su ayuda ?e1071::skewness para
+  #     interpretacion.
   #
   # n.auto = Logico. Si TRUE (por defecto), la propia funcion detectara si hay valores perdidos.
   #       Si los encuentra, entregara tres cantidades: n.total, n.na y n. Si no los encuentra solo
@@ -44,7 +37,10 @@ estad.descrip <- function(x, r = NULL, var.en.filas = T,
   #       moda = Valor con la frecuencia mas alta. Si existen dos o mas valores con la misma frecuencia
   #              mas alta, se promedian para entregar uno solo. Si n.dist = n, moda = media.
   #       n.moda = frecuencia absoluta correspondiente a la moda.
+  # 
+  # print_tibble = Si TRUE, el resultado se imprime como un tibble y no como un data.frame (por defecto)
   
+  # CODIGO:
   # Funcion para verificar si un paquete(s) ya esta instalado, y si no, que lo instale(s)
   check.packages <- function(pkg){
     # pkg = vector de character con los nombres de los paquetes a verficarse e instalarse
@@ -64,22 +60,16 @@ estad.descrip <- function(x, r = NULL, var.en.filas = T,
   }
   
   # Mensajes de parada por ingreso incorrecto de argumentos:
-  posibles.est <- c('n.m.s', 'n.m.s.cv', 'n.r.m.s', 'n.r.m.s.cv', 'todo', 'rob', 'quant')
   if( !(is.vector(x) | is.data.frame(x)) ) stop('x debe ser un vector o un data.frame')
   tipo.x <- if(is.vector(x)) is.numeric(x) else{
     w <- sapply(as.data.frame(x[, r]), is.numeric )
     sum(w) == length(r)
   }
   if( !tipo.x ) stop('x debe ser un vector numerico, o un data.frame con columnas numericas indicadas con el argumento r')
-  if( ! est[1] %in% posibles.est ) {
-    stop(paste0("El argumento 'est' debe ser uno de: ", posibles.est) )
-  }
   
   # Funcion que calcula estadisticos y opera sobre un vector y controla
   # el tipo del resultado:
-  fun1 <- function(x, tipo.res = c('v', 'd'), 
-                   est = c('n.m.s', 'n.m.s.cv', 'n.r.m.s', 'n.r.m.s.cv', 'todo', 'rob', 'quant'), 
-                   auto.n = T, n.dist = F, ...){
+  fun1 <- function(x, tipo.res = c('v', 'd'), auto.n = T, n.dist = F, estad_forma = F, ...){
     
     # Calculo de estadisticos:
     n.total <- length(x)
@@ -95,12 +85,7 @@ estad.descrip <- function(x, r = NULL, var.en.filas = T,
       cv <- ifelse(n >= 2, round(s/m*100,2), NA)
       q <- fivenum(x, ...)
       names(q) <- c('min', 'q25', 'q50', 'q75', 'max')
-      x1 <- x[!is.na(x)]
-      # mg <- prod(x1)^(1/n)      # media geometrica
-      mg <- ifelse(min(x1) <= 0, NA, exp( mean(log(x1)) )  )     # media geometrica
-      mad <- median( abs(as.numeric(q['q50']) - x1) )   # desv. mediana absoluta
-      cvr <- round(mad / as.numeric(q['q50']) *100, 2)  # coef. de variacion robusto
-      if(est == 'todo') {
+      if(estad_forma){
         check.packages(pkg = 'e1071')
         sesgo <- ifelse(n >= 4,  e1071::skewness(x, type = 2, ...), NA)
         kurt  <- ifelse(n >= 4,  e1071::kurtosis(x, type = 2, ...), NA)
@@ -110,19 +95,14 @@ estad.descrip <- function(x, r = NULL, var.en.filas = T,
       q <- rep(NA, 5)
       names(q) <- c('min', 'q25', 'q50', 'q75', 'max')
     }
-
-
+    
+    
     # Organizacion de resultados en un vector de acuerdo al valor del argumento 'est':
-    res <- switch(est[1],
-                  todo = c(q, media = m, de = s, cv = cv, sesgo = sesgo, kurt = kurt,
-                           media.geom = mg, mad = mad, cvr = cvr),
-                  rob = c(q, media.geom = mg, mad = mad, cvr = cvr),
-                  n.m.s  = c(media = m, de = s),
-                  n.r.m.s = c(q['min'], q['max'], media = m, de = s),
-                  n.m.s.cv = c(media = m, de = s, cv = cv),
-                  n.r.m.s.cv = c(q['min'], q['max'], media = m, de = s, cv = cv),
-                  quant  = q
-    )
+    res <- if(estad_forma){
+      c(q, media = m, de = s, cv = cv, sesgo = sesgo, kurt = kurt)
+    } else{
+      c(q, media = m, de = s, cv = cv)
+    }
     
     # Funcion para calcular moda y n.moda:
     moda <- function(x){
@@ -144,15 +124,15 @@ estad.descrip <- function(x, r = NULL, var.en.filas = T,
     # del tamano de muestra contenida en res.n o incluso adicionando tambien
     # informacion de frecuencias relevantes y moda dependiendo del argumento logico n.dist
     res <- if(n.dist){
-        x1 <- x[!is.na(x)]
-        n_dist  <- ifelse(n > 0, length(unique(x1)), 0)    # nro. de valores distintos
-        n_ceros <- ifelse(n > 0, sum(x1 == 0), 0)          # nro. de ceros
-        r.moda <- moda(x)
-        ns <- c(n.dist = n_dist, n.ceros = n_ceros, r.moda)  # se guarda todo
-        c(res.n, ns, res)   # se entrega vector actualizado
-        } else{
-          c(res.n, res)       # se entrega vector actualizado
-        } 
+      x1 <- x[!is.na(x)]
+      n_dist  <- ifelse(n > 0, length(unique(x1)), 0)    # nro. de valores distintos
+      n_ceros <- ifelse(n > 0, sum(x1 == 0), 0)          # nro. de ceros
+      r.moda <- moda(x)
+      ns <- c(n.dist = n_dist, n.ceros = n_ceros, r.moda)  # se guarda todo
+      c(res.n, ns, res)   # se entrega vector actualizado
+    } else{
+      c(res.n, res)       # se entrega vector actualizado
+    } 
     
     # Entrega el resultado en un vector o data.frame:
     switch(tipo.res[1],
@@ -168,22 +148,24 @@ estad.descrip <- function(x, r = NULL, var.en.filas = T,
   if(is.vector(x) | length(r) == 1 ){
     # Si x es un vector o un data.frame y una sola columna
     if(is.vector(x))  {
-      res <- fun1(x = x, tipo.res = 'd', na.rm = T, est = est[1], auto.n = n.auto, n.dist = n.dist)
+      res <- fun1(x = x, tipo.res = 'd', na.rm = T, auto.n = n.auto, n.dist = n.dist, estad_forma = estad_forma)
     } else{
-      res <- fun1(x = as.numeric(unlist(x[, r])), tipo.res = 'd', na.rm = T, 
-                  est = est[1], auto.n = n.auto, n.dist = n.dist)
+      res <- fun1(x = as.numeric(unlist(x[, r])), tipo.res = 'd', na.rm = T,  
+                  auto.n = n.auto, n.dist = n.dist, estad_forma = estad_forma)
     }
-   # Si x es un data.frame y r indica 2 o mas columnas 
+    # Si x es un data.frame y r indica 2 o mas columnas 
   } else{
     y <- as.data.frame(x[, r])
     col.na <- any(apply(y, 2, is.na))
     if(var.en.filas){
-      res <- t(apply(y, 2, fun1, tipo.res = 'v', na.rm = T, est = est[1], auto.n = ifelse(n.auto, !col.na, F), n.dist = n.dist) )
+      res <- t(apply(y, 2, fun1, tipo.res = 'v', na.rm = T, auto.n = ifelse(n.auto, !col.na, F), 
+                     n.dist = n.dist, estad_forma = estad_forma) )
       rownames(res) <- NULL
-      res0 <- data.frame( var = names(x[, r]))
+      res0 <- data.frame( variable = names(x[, r]))
       res <- cbind(res0, res)
     } else{
-      res <- apply(y, 2, fun1, tipo.res = 'v', na.rm = T, est = est[1],  auto.n = ifelse(n.auto, !col.na, F), n.dist = n.dist )
+      res <- apply(y, 2, fun1, tipo.res = 'v', na.rm = T,  auto.n = ifelse(n.auto, !col.na, F), 
+                   n.dist = n.dist, estad_forma = estad_forma )
       nom.est <- rownames(res)
       rownames(res) <- NULL
       res0 <- data.frame( est = nom.est )
@@ -192,13 +174,12 @@ estad.descrip <- function(x, r = NULL, var.en.filas = T,
   }
   
   # Se entrega el resultado dependiendo del argumento 'print':
-  switch(print[1],
-         data.frame = res,
-         tibble = {
-           check.packages(pkg = 'tibble')
-           tibble::as_tibble(res)
-         }
-  )
+  if(print_tibble){
+    check.packages(pkg = 'tibble')
+    tibble::as_tibble(res)
+  } else{
+    as.data.frame(res)
+  }
   
   # VALOR:
   # Entrega un data.frame donde las filas representan variables o estadisticos
@@ -208,49 +189,35 @@ estad.descrip <- function(x, r = NULL, var.en.filas = T,
   # library(MASS)
   # data(birthwt)
   # Sobre un vector:
-  # estad.descrip(x = birthwt$bwt)
-  # estad.descrip(x = c(birthwt$bwt, NA, NA, NA))   # con datos perdidos
-  # estad.descrip(x = birthwt$bwt,  est = 'todo')
-  # estad.descrip(x = birthwt$bwt,  est = 'todo', n.auto = F)
-  # estad.descrip(x = c(birthwt$bwt, NA,NA, NA),  est = 'todo')  # con datos perdidos
-  # estad.descrip(x = c(birthwt$bwt, NA,NA, NA),  est = 'otra.cosa')  # valiando argumento 'est'
-  # estad.descrip(x = as.character(birthwt$bwt),  est = 'todo')  # valiando argumento 'x'
-  # estad.descrip(x = birthwt$bwt,  est = 'quant')  # solo cuartiles y n
-  # estad.descrip(x = c(birthwt$bwt, NA,NA, NA),  est = 'quant')  # solo cuartiles y n, con datos perdidos
-  # estad.descrip(x = c(birthwt$bwt, NA,NA, NA),  est = 'rob')  # solo robustos y n, con datos perdidos
-  # estad.descrip(x = birthwt$bwt,  est = 'rob')  # solo robustos y n, sin datos perdidos
+  # estad_descrip(x = birthwt$bwt)
+  # estad_descrip(x = birthwt$bwt, estad_forma = T)
+  # estad_descrip(x = c(birthwt$bwt, NA, NA, NA))   # con datos perdidos
+  # estad_descrip(x = birthwt$bwt, n.auto = F)
+  # estad_descrip(x = as.character(birthwt$bwt))  # valiando argumento 'x'
   
   # Sobre un data.frame (sin datos perdidos):
-  # estad.descrip(x = birthwt)   # por defecto r = NULL, que indica todas las columnas
-  # estad.descrip(x = birthwt, n.auto = F)   # por defecto r = NULL, que indica todas las columnas
-  # estad.descrip(x = birthwt, r = 'bwt')
-  # estad.descrip(x = birthwt, r = c('bwt', 'age') )   # dos columnas
-  # estad.descrip(x = birthwt, r = c('bwt', 'age'), var.en.filas = F )   # dos columnas y var en columnas
-  # estad.descrip(x = birthwt, r = c('bwt', 'age'), var.en.filas = F, est = 'todo' )
-  # estad.descrip(x = birthwt, r = c('bwt', 'age'), var.en.filas = F, est = 'todo', n.auto = F )
-  # estad.descrip(x = birthwt, r = c('bwt', 'age'), var.en.filas = T, est = 'todo' )
-  # estad.descrip(x = birthwt, r = c('bwt', 'age'), var.en.filas = T, est = 'rob' )  # solo estad. robustos
-  # estad.descrip(x = birthwt, r = c('bwt', 'age'), var.en.filas = F, est = 'rob' )  # solo estad. robustos
-  # estad.descrip(x = birthwt, r = c('bwt', 'age'), var.en.filas = T, est = 'quant' )  # solo cuartiles
-  # estad.descrip(x = birthwt, r = c('bwt', 'age', 'ftv'), var.en.filas = F, est = 'quant' )  # solo cuartiles, tres variables
+  # estad_descrip(x = birthwt)   # por defecto r = NULL, que indica todas las columnas
+  # estad_descrip(x = birthwt, n.auto = F)   # por defecto r = NULL, que indica todas las columnas
+  # estad_descrip(x = birthwt, r = 'bwt')
+  # estad_descrip(x = birthwt, r = c('bwt', 'age') )   # dos columnas
+  # estad_descrip(x = birthwt, r = c('bwt', 'age'), var.en.filas = F )   # dos columnas y var en columnas
   
   # Sobre un data.frame con datos perdidos:
   # datos1 <- data.frame(x = c(35.5, NA, 10.9, 25.8, NA), y = c(5.67, 8.91, NA, 6.34, 2.89))
   # datos2 <- data.frame(x = c(35.5, NA, 10.9, 25.8, NA), y = c(5.67, 8.91, 7.34, 6.34, 2.89))
-  # estad.descrip(x = datos1, var.en.filas = T)  
-  # estad.descrip(x = datos1,  var.en.filas = F)   # no muy conveniente por el tema de decimales
-  # estad.descrip(x = datos1,  var.en.filas = T, est = 'rob')  
-  # estad.descrip(x = datos2,  var.en.filas = T, est = 'rob')  # una columna con datos perdidos y otra sin datos perdidos
-  # estad.descrip(x = datos2,  r = 1, var.en.filas = T, est = 'rob')
-  # estad.descrip(x = datos2,  r = 2, var.en.filas = T, est = 'rob')
-  # estad.descrip(x = datos2,  r = 2, var.en.filas = T, est = 'rob', n.auto = F)
-  # estad.descrip(x = datos2,  r = 2, var.en.filas = T, est = 'todo', n.auto = F)
+  # estad_descrip(x = datos1) 
+  # estad_descrip(x = datos1, estad_forma = T) 
+  # estad_descrip(x = datos1,  var.en.filas = F)   # no muy conveniente por el tema de decimales
+  # estad_descrip(x = datos1,  var.en.filas = F, estad_forma = T)   # no muy conveniente por el tema de decimales
+  # estad_descrip(x = datos1, n.dist = T)
+  # estad_descrip(x = datos1, n.dist = T, print_tibble = T)
   
   # Usando ddply (de plyr):
   # require(plyr)
-  # plyr::ddply(birthwt,  ~ smoke + race, estad.descrip, r =  'bwt', est = 'n.m.s', var.en.filas = T)
-  # plyr::ddply(birthwt,  ~ smoke + race, estad.descrip, r =  'bwt', est = 'rob', var.en.filas = T)   # solo robustos
-  # plyr::ddply(birthwt,  ~ smoke + race, estad.descrip, r =  'bwt', est = 'todo', var.en.filas = T)   # todo
-  # plyr::ddply(birthwt,  ~ smoke + race, estad.descrip, r =  c('bwt', 'age'), est = 'n.m.s', var.en.filas = T)
-  # plyr::ddply(birthwt,  ~ smoke + race, estad.descrip, r =  c('bwt', 'age'), est = 'n.m.s', var.en.filas = F)
+  # plyr::ddply(birthwt,  ~ smoke + race, estad_descrip, r =  'bwt')
+  # plyr::ddply(birthwt,  ~ smoke + race, estad_descrip, r =  c('bwt', 'age'))
+  # plyr::ddply(birthwt,  ~ smoke + race, estad_descrip, r =  c('bwt', 'age'), var.en.filas = F)
+  # plyr::ddply(birthwt,  ~ smoke + race, estad_descrip, r =  c('bwt', 'age'), var.en.filas = F, estad_forma = T)
+  # plyr::ddply(birthwt,  ~ smoke + race, estad_descrip, r =  c('bwt', 'age'), 
+  #             var.en.filas = F, estad_forma = T, print_tibble = T)     # no funciona print_tibble dado que 'ddply' entrega un data.frame 
 }
